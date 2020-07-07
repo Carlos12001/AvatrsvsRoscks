88b60ecfc5402f543403a9dc7b05ec447924e483
Apollo13 <calderon.ig08@estudiantec.cr>
2020-07-07 11:46:29 -0600

    add entry code

--- Ventanas/ventana_nuevo_nombre.py
+++ Ventanas/ventana_nuevo_nombre.py
@@ -3,10 +3,73 @@ from GameV0 import *
 
 # --------------------------------------------- Clases ---------------------------------------#
 
-class Entry():
-    def __init__ (self, x, y, width, height, text = ""):
-        self.rect = pygame
+#color_inactive = brown
+#color_active = white
 
+#class Entry():
+ #   def __init__(self, x, y, width, height, text = ""):
+  #      self.rect = pygame.Rect(x,y,width, height)
+   #     self.color = color_inactive
+    #    self.text = text
+     #   self.txtsurface = font2.render(text,True,self.color)
+      #  self.active = False
+
+    #def type_entry (self, event):
+     #   if event.type == pygame.MOUSEBUTTONDOWN:
+      #      if self.rect.collidepoint(event.pos): # Cuando el usuario presione el cuadro
+       #         self.active = not self.active # Se activa el color
+        #    else:
+         #       self.active = False
+          #  self.color = color_active # cambia el color de la caja de txt
+        #elif event.type == pygame.K_RETURN:
+         #   if self.active:
+          #      if event.key == pygame.K_RETURN:
+           #         print(self.text)
+            #        self.text = ""
+             #   elif event.key == pygame.K_BACKSPACE:
+              #      self.text = self.text[:-1]
+               # else:
+                #    self.text += event.unicode
+                #self.txt_surface = font2.render(self.text, True, self.color)
+
+    #def update(self): # Acomoda el cuadro en caso de ser un texto muy largo
+     #   width = max(200, self.txt_surface.get_width() + 10)
+      #  self.rect.w = width
+
+    #def draw(self, screen):
+     #   screen.blit(self.txtsurface, (self.rect.x + 5, self.rect.y + 5))
+      #  pygame.draw.rect(screen, self.color, self.rect, 2)
+
+class Entry:
+    def __init__(self,x,y):
+        self.line = 0 # Mostrar en cada linea una letra
+        self.letters = ["",] # Mostrar caracteres
+        self.font = pygame.font.Font(None, 25)
+        self.distance = 20
+        self.posX = x
+        self.posY = y
+    def keys(self, event):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.type == pygame.K_RETURN:
+                    self.letters.append("")
+                    self.line += 1
+                elif event.type == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.K_BACKSPACE:
+                    if self.letters[self.line] == "" and self.line > 0:
+                        self.letters = self.letters[0: -1]
+                        self.line -= 1
+                    else:
+                        self.letters[self.line] = self.letters[self.line][0:-1]
+                else:
+                    self.letters[self.line] = strg(self.letters[self.line] + event.unicode)
+    def mensaje (self, surface):
+        screen.fill(brown)
+        for self.line in range(len(self.letters)):
+            letter_render = self.font.render(self.letters[self.line], True, white)
+        screen.blit(letter_render, (self.posX, self.posY + self.distance * self.line))
 # --------------------------------------------- Funciones ---------------------------------------------- #
 # -------------------------------------- Funcion para crear texto -------------------------------------- #
 
@@ -19,7 +82,9 @@ def text(text, font, color, surface, x, y):
 
 # ---------------------- Ventana dodne el jugador digita su nombre por primera vez --------------------- #
 
+
 def name():
+    name_entry = Entry(300,400)
     screen.fill(dark)  # color la ventana
     text("Ingrese su nombre", font, darkpurple, screen, 505, 105)
     text("Ingrese su nombre", font, green, screen, 500, 100)
@@ -33,6 +98,12 @@ def name():
             if event.type == pygame.KEYUP:
                 run = False
                 from Ventanas import ventana_de_menu
+                #run = False
+        name_entry.keys(pygame.event.get())
+        name_entry.mensaje(screen)
+
+
+
         # entrada texto del nombre
         # escribir el nombre del jugador en un txt
 
