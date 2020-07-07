bf0a7ece9f81f10e37415fc8407956fdbda50100
Apollo13 <calderon.ig08@estudiantec.cr>
2020-07-07 12:09:42 -0600

    Add rooks.py

---
+++ RooksV0.py
@@ -0,0 +1,14 @@
+
+class Rooks ():
+    def __init__(self, x, y, type):
+        self.x = x
+        self.y = y
+        self.type = type
+
+
+    def position (self, )
+        # rango del mouse
+
+
+    def rook_position (self):
+        # colocar rook en po
