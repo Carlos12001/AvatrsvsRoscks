a8f5805f158a427d56a61b0798eac31a80edcc96
Carlos12001 <carlos.andres12001@gmail.com>
2020-07-07 14:51:52 -0600

    Logica Para crear Avarts

---
+++ Objetos/avatars_file.py
@@ -0,0 +1,74 @@
+import pygame,sys,random
+from GameV0 import *
+class New_Avart ( pygame.sprite.Sprite ):
+    def __init__( self, type_avatar ):
+        super().__init__()
+
+        self.type_avatar = type_avatar
+        #Que tipo de Avatar va crear
+
+        #Arquero
+        if self.type_avatar == 1 :
+            #Craracteristicas del pygame
+            self.image = pygame.image.load( 'resource/avatar_flechador.png' ).convert()
+            self.image.set_colorkey( white )
+            self.rect = self.image.get_rect()
+            self.rect.x = random.choice( range( 250, 750 ,100 ) )
+            self.rect.y = size[1]-100
+
+            #Caracteristicas del arquero
+            self.ps = 5
+            self.pa = 5
+
+            self.speed_walk = 2
+            self.speed_atack = 1.25
+
+        #Escudero
+        elif self.type_avatar == 2 :
+            # Craracteristicas del pygame
+            self.image = pygame.image.load('resource/avatar_escudero.png').convert()
+            self.image.set_colorkey(white)
+            self.rect = self.image.get_rect()
+            self.rect.x = random.choice(range(250, 750, 100))
+            self.rect.y = size[1] - 100
+
+            # Caracteristicas del escudero
+            self.ps = 5
+            self.pa = 5
+
+            self.speed_walk = 2
+            self.speed_atack = 1.25
+
+        #Lenador
+        elif self.type_avatar == 3 :
+            # Craracteristicas del pygame
+            self.image = pygame.image.load('resource/avatar_lenador.png').convert()
+            self.image.set_colorkey(white)
+            self.rect = self.image.get_rect()
+            self.rect.x = random.choice(range(250, 750, 100))
+            self.rect.y = size[1] - 100
+
+            # Caracteristicas del lenador
+            self.ps = 5
+            self.pa = 5
+
+            self.speed_walk = 2
+            self.speed_atack = 1.25
+
+        #Canival
+        elif self.type_avatar == 4:
+            # Craracteristicas del pygame
+            self.image = pygame.image.load('resource/avatar_canival.png').convert()
+            self.image.set_colorkey(white)
+            self.rect = self.image.get_rect()
+            self.rect.x = random.choice(range(250, 750, 100))
+            self.rect.y = size[1] - 100
+
+            # Caracteristicas del canival
+            self.ps = 5
+            self.pa = 5
+
+            self.speed_walk = 2
+            self.speed_atack = 1.25
+    def update(self):
+        self.rect.y -= 100
