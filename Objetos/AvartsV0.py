fe64ecfd41d3ad5c9b27b3638b8e4b6233ec202d
Carlos12001 <carlos.andres12001@gmail.com>
2020-07-08 08:50:24 -0600

    Funciones para comprar que tipo Avartar es

--- Objetos/AvartsV0.py
+++ Objetos/AvartsV0.py
@@ -1,5 +1,11 @@
 import pygame,sys,random
 from GameV0 import *
+
+#PRimero funcion crear dibuja en cada matriz y revisa que no halla ningun avatar dicha matriz
+#Avatars lista mpara saber que tiempo fue puesto cada avatar y para saber cuanto tiempo a pasado el avatar hasta
+#Cuando fue puesto y se hace (tiempo actual - tiempo avatar)
+
+
 class New_Avart ( pygame.sprite.Sprite ):
     def __init__( self, type_avatar ):
         super().__init__()
@@ -70,5 +76,25 @@ class New_Avart ( pygame.sprite.Sprite ):
 
             self.speed_walk = 2
             self.speed_atack = 1.25
+
+    #Obtener posicion en X, y
+    def posicion_get(self):
+        return (self.rect.x,self.rect.y)
+    #Obtener nombre del tipo de enemigo
+    def type_get(self):
+        if self.type_avatar == 1:
+            return 'Arquero'
+        elif self.type_avatar == 2:
+            return 'Escudero'
+        elif self.type_avatar == 3:
+            return 'Lenador'
+        elif self.type_avatar == 4:
+            return 'Canival'
+
+
+    #Obtener imagen
+    def image_get(self):
+        return self.image
+    #Movimiento
     def update(self):
         self.rect.y -= 100
